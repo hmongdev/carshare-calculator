08104dbcffcdf5525c00e5e8992b83a695f1fe39
Avery Yang <averyyang2674@gmail.com>
2022-12-06 21:36:54 -0600

    complete calculator

--- src/App.js
+++ src/App.js
@@ -41,49 +41,49 @@ const App = () => {
         },
         {
             id: 1,
-            name: 'Try It',
+            name: 'EV-Try It',
             perMinute: 0.3,
             perHour: 15.0,
             perDay: 110.0,
         },
         {
             id: 2,
-            name: 'Everyday',
+            name: 'EV-Everyday',
             perMinute: 0.2,
             perHour: 9.75,
             perDay: 71.5,
         },
         {
             id: 3,
-            name: 'Everyday PLUS',
+            name: 'EV-Everyday PLUS',
             perMinute: 0.2,
             perHour: 9.75,
             perDay: 71.5,
         },
         {
             id: 4,
-            name: 'Adventure PLUS',
+            name: 'EV-Adventure PLUS',
             perMinute: 0.18,
             perHour: 9.0,
             perDay: 66.0,
         },
         {
             id: 5,
-            name: 'Access PLUS',
+            name: 'EV-Access PLUS',
             perMinute: 0.18,
             perHour: 9.0,
             perDay: 66.0,
         },
         {
             id: 6,
-            name: 'Student',
+            name: 'EV-Student',
             perMinute: 0.18,
             perHour: 9.0,
             perDay: 66.0,
         },
         {
             id: 7,
-            name: 'Student PLUS',
+            name: 'EV-Student PLUS',
             perMinute: 0.18,
             perHour: 9.0,
             perDay: 66.0,
@@ -97,49 +97,49 @@ const App = () => {
         },
         {
             id: 1,
-            name: 'Try It',
+            name: 'HC-Try It',
             perMinute: 0.17,
             perHour: 10.0,
             perDay: 75.0,
         },
         {
             id: 2,
-            name: 'Everyday',
+            name: 'HC-Everyday',
             perMinute: 0.11,
             perHour: 6.5,
             perDay: 48.75,
         },
         {
             id: 3,
-            name: 'Everyday PLUS',
+            name: 'HC-Everyday PLUS',
             perMinute: 0.11,
             perHour: 6.5,
             perDay: 48.75,
         },
         {
             id: 4,
-            name: 'Adventure PLUS',
+            name: 'HC-Adventure PLUS',
             perMinute: 0.1,
             perHour: 9.0,
             perDay: 45.0,
         },
         {
             id: 5,
-            name: 'Access PLUS',
+            name: 'HC-Access PLUS',
             perMinute: 0.1,
             perHour: 9.0,
             perDay: 45.0,
         },
         {
             id: 6,
-            name: 'Student',
+            name: 'HC-Student',
             perMinute: 0.1,
             perHour: 9.0,
             perDay: 45.0,
         },
         {
             id: 7,
-            name: 'Student PLUS',
+            name: 'HC-Student PLUS',
             perMinute: 0.1,
             perHour: 9.0,
             perDay: 45.0,
@@ -163,13 +163,15 @@ const App = () => {
         }
     };
 
-    const calculateCosts = (minutes, hours, days, cityTax) => {
+    const calculateCosts = (userMinutes, userHours, userDays, cityTax) => {
+        // check if 1st choice is empty
         if (cityTax === 0) {
             Swal.fire({
                 icon: 'error',
                 title: 'Oops...',
                 text: 'Select Your City!',
             });
+            // check if 2nd choice is empty
         } else if (
             carPlan.length === 0 ||
             carPlan === '--- Choose EVIE Plan ---' ||
@@ -180,20 +182,30 @@ const App = () => {
                 title: 'Oops...',
                 text: 'Select A Car Plan!',
             });
+            // if days === 0 && greater than 7.5 hours
+        } else if (userDays === 0 && userHours >= 7 && userMinutes >= 30) {
+            let cost = carPlan.perDay * 1;
+            setPreTax(cost);
+            setTaxCost(Number((cityTax * cost).toFixed(2)));
+            // if days >= 1 && greater than 7.5 hours
+        } else if (userDays >= 1 && userHours >= 7 && userMinutes >= 30) {
+            let cost = carPlan.perDay * (userDays + 1);
+            setPreTax(cost);
+            setTaxCost(Number((cityTax * cost).toFixed(2)));
         } else {
-            let minuteCost = minutes * carPlan.perMinute;
-            let hourCost = hours * carPlan.perHour;
-            let dayCost = days * carPlan.perDay;
+            // actual calculation
+            alert(`actual calculation`);
+            let minuteCost = userMinutes * carPlan.perMinute;
+            let hourCost = userHours * carPlan.perHour;
+            let dayCost = userDays * carPlan.perDay;
             let cost = minuteCost + hourCost + dayCost;
             setPreTax(cost);
             setTaxCost(Number((cityTax * cost).toFixed(2)));
         }
     };
 
-    console.log(`inside carPlan`, carPlan);
-
     return (
-        <div className="w-full h-screen bg-gradient-to-b from-orange-500 via-transparent to-green-600">
+        <div className="flex flex-col w-full h-full bg-gradient-to-b from-orange-500 via-transparent to-green-600">
             <div className="flex justify-evenly w-full py-3">
                 <img src={evieLogo} alt="evie logo" className="w-1/2" />
                 <img src={hourCarLogo} alt="hourcar logo" className="w-1/2" />
@@ -211,7 +223,7 @@ const App = () => {
                             key={id}
                             value={tax}
                             onClick={(e) => setCityTax(e.target.value)}
-                            className="bg-blue-900 text-white rounded-full mx-auto px-2 border-2 border-gray-500 w-[30%]"
+                            className="bg-blue-900 text-white rounded-full mx-auto px-2 border-2 border-gray-500 w-[50vw]"
                         >
                             {name}
                         </button>
@@ -226,7 +238,7 @@ const App = () => {
                     <button
                         onClick={(e) => setCarType(e.target.value)}
                         value="Evie"
-                        className="bg-green-600 text-white rounded-full mx-auto px-2 border-2 border-gray-500 w-[30%]"
+                        className="bg-green-600 text-white rounded-full mx-auto px-2 border-2 border-gray-500 w-[30vw]"
                     >
                         Evie
                     </button>
@@ -241,10 +253,14 @@ const App = () => {
                 {carType === 'HOURCAR' ? (
                     <select
                         onChange={selectPlan}
-                        className="flex mx-auto w-4/5 gap-5 rounded-lg py-1 px-3"
+                        className="flex mx-auto w-4/5 border-2 gap-5 rounded-lg py-1 px-3"
                     >
                         {hourCarPlans.map(({ id, name }) => (
-                            <option key={id} value={name}>
+                            <option
+                                key={id}
+                                value={name}
+                                className="text-center"
+                            >
                                 {name}
                             </option>
                         ))}
@@ -252,10 +268,14 @@ const App = () => {
                 ) : (
                     <select
                         onChange={selectPlan}
-                        className="flex mx-auto w-4/5 gap-5 rounded-lg py-1 px-3"
+                        className="flex mx-auto w-4/5 border-2 gap-5 rounded-lg py-1 px-3"
                     >
                         {eviePlans.map(({ id, name }) => (
-                            <option key={id} value={name}>
+                            <option
+                                key={id}
+                                value={name}
+                                className="text-center"
+                            >
                                 {name}
                             </option>
                         ))}
@@ -267,7 +287,10 @@ const App = () => {
                     3. Rental Duration
                 </h1>
                 <div className="flex flex-col w-3/5 gap-3 mx-auto">
-                    <select onChange={(e) => setDays(e.target.value)}>
+                    <select
+                        onChange={(e) => setDays(Number(e.target.value))}
+                        className="border-2"
+                    >
                         {days.map((day, i) => {
                             return (
                                 <option key={i} value={day}>
@@ -276,7 +299,10 @@ const App = () => {
                             );
                         })}
                     </select>
-                    <select onChange={(e) => setHours(e.target.value)}>
+                    <select
+                        onChange={(e) => setHours(Number(e.target.value))}
+                        className="border-2"
+                    >
                         {hours.map((hour, i) => {
                             return (
                                 <option key={i} value={hour}>
@@ -285,7 +311,10 @@ const App = () => {
                             );
                         })}
                     </select>
-                    <select onChange={(e) => setMinutes(e.target.value)}>
+                    <select
+                        onChange={(e) => setMinutes(Number(e.target.value))}
+                        className="border-2"
+                    >
                         {minutes.map((minute, i) => {
                             return (
                                 <option key={i} value={minute}>
@@ -303,33 +332,31 @@ const App = () => {
                                 cityTax
                             )
                         }
-                        className="bg-blue-900 text-white rounded-full mx-auto px-2 border-2 border-gray-500 w-fit"
+                        className="bg-blue-900 text-white rounded-full mx-auto px-2 border-2 border-gray-500 w-[50vw]"
                     >
                         Calculate
                     </button>
                 </div>
             </div>
 
-            <div className="grid grid-cols-3 font-bold w-[90%] mx-auto my-10 text-xl">
+            <div className="grid grid-cols-3 font-bold w-[90%] mx-auto py-10 text-xl">
                 <ul>
                     <li>City Taxes</li>
                     <li>Car Plan</li>
-                    <hr className="border-black" />
-                    <li>Total Cost</li>
+                    <li>You Pay</li>
                 </ul>
-                <ul className="whitespace-nowrap">
-                    <li className="text-center text-red-700">
+                <ul className="">
+                    <li className="text-center text-red-800">
                         {(cityTax * 100).toFixed(3)}%
                     </li>
-                    <li className="text-center text-blue-700">
+                    <li className="flex justify-center whitespace-nowrap text-sm text-blue-700">
                         {carPlan.id !== 0 && <p>{carPlan.name}</p>}
                     </li>
-                    <hr className="border-black" />
                 </ul>
                 <ul>
-                    <li className="text-right">${taxCost.toFixed(2)}</li>
-                    <li className="text-right">${preTax.toFixed(2)}</li>
-                    <li className="text-right text-red-700">
+                    <li className="text-right">{taxCost.toFixed(2)}</li>
+                    <li className="text-right">{preTax.toFixed(2)}</li>
+                    <li className="text-right text-red-800">
                         <hr className="border-black" />
                         -${(preTax + taxCost).toFixed(2)}
                     </li>
